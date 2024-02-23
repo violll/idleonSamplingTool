4b874092e08e7af91887765935a40366b394bf87
violll <tri.tardis@gmail.com>
2024-02-23 14:15:12 -0500

    feat: add check for vial mats

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ class IdleonSamplingTool:
         user = User()
 
         # ask for refinery and vial mobs here
-        self.refinery = self.checkRefineryMobs()
+        self.refineryMobs = self.checkRefineryMobs()
+        self.vialMobs = self.getVialMats()
     
     def checkRefineryMobs(self):
         refineryMobs = input("Would you like to sample mobs for refinery? (GMush, Bullfrog, Sand, Sheep, Thermometer, Neptuneeyes)\n> ")
         if refineryMobs in ["True", "Yes", "Y"]: return True
         else: return False
+
+    def getVialMats(self):
+        vialMats = input("Which vials would you like to focus samples on? Type them separated by commas. If you do not have any vials you wish to focus on, press enter.\n> ")
+        if vialMats == "": return []
+        else:
+            return vialMats.split(", ")
 
 if __name__ == "__main__":
     IdleonSamplingTool()
