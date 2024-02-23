06937207d9ceea01c76e5391953908a091a44137
violll <tri.tardis@gmail.com>
2024-02-23 16:41:01 -0500

    feat: basic sample algorithm implemented, including filling extra sample slots with useful mats found in "miscMats" in the .json file

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ class IdleonSamplingTool:
     def __init__(self) -> None:
         user = User()
 
-        # ask for refinery and vial mobs here
+        # ask for refinery mobs and vial mats here
         self.refineryMobs = self.checkRefineryMobs()
-        self.vialMobs = self.getVialMats()
+        self.vialMats = self.getVialMats()
     
     def checkRefineryMobs(self):
-        refineryMobs = input("Would you like to sample mobs for refinery? (GMush, Bullfrog, Sand, Sheep, Thermometer, Neptuneeyes)\n> ")
+        refineryMobs = input("Would you like to sample mobs for refinery? (SporeCap, BullfrogHorn, PocketSand, FloofPloof, CrackedGlass, ContactLense)\n> ")
         if refineryMobs in ["True", "Yes", "Y"]: return True
         else: return False
 
--- toolboxDataCollector.py
+++ toolboxDataCollector.py
@@ -1,5 +1,6 @@
 import json
 import pickle
+import copy
 
 class UserData():
     def __init__(self) -> None:
@@ -10,8 +11,8 @@ class UserData():
         charNames = self.userData['charNames']
 
         # character classes
-        self.chars = {charNames[i]: self.toolboxKeys["classesKey"][str(self.userData['data']['CharacterClass_' + str(i)])] for i in range(len(charNames))}
-        print(self.chars)
+        self.chars = {charNames[i]: copy.deepcopy(self.toolboxKeys["classesKey"][str(self.userData['data']['CharacterClass_' + str(i)])]) for i in range(len(charNames))}
+
         # 3d printer slots data
         printer = [d for d in eval(self.userData['data']['Print']) if type(d) != int and d != 'Blank']
         self.nPrinterSlots = len(printer)//len(charNames)
@@ -25,6 +26,77 @@ class UserData():
         self.logs = [self.toolboxKeys["logsKey"][log] for log in storage if ("Tree" in log or "Foal" in log) and "Interior" not in log]
         self.bugs = [self.toolboxKeys["bugsKey"][bug] for bug in storage if "Bug" in bug]
         self.fish = [self.toolboxKeys["fishKey"][fish] for fish in storage if "Fish" in fish and "Food" not in fish]
+
+        self.sample()
+
+    def sample(self, vials=["HermitCan", "SporeCap", "MapleLogs", "SnakeSkin"]):
+        # add mats/mobs to assigned once they are checked off
+        assigned = set()
+
+        # assign refinery
+        ## refinery mobs
+        ## assumes there is one character assigned to RefineryMobs and they have enough sample slots
+        char = [self.chars[char] for char in self.chars if self.chars[char]["SampleRole"] == "RefineryMobs"][0]
+        char["Samples"].extend(self.toolboxKeys["refineryMobs"])
+
+        ## refinery mats
+        for mat in self.toolboxKeys["refineryMats"]: 
+            matType = self.getMatType(mat)
+            char = [self.chars[char] for char in self.chars if self.chars[char]["SampleRole"] == matType and len(self.chars[char]["Samples"]) < self.nPrinterSlots][-1]
+            char["Samples"].append(mat)
+
+            if mat not in vials and mat not in self.toolboxKeys["atomMatSources"]:
+                assigned.add(mat)
+
+        # assign atom generating mats
+        for mat in self.toolboxKeys["atomMatSources"]:
+            matType = self.getMatType(mat)
+            chars = [self.chars[char] for char in self.chars if self.chars[char]["SampleRole"] == matType and len(self.chars[char]["Samples"]) < self.nPrinterSlots and mat not in self.chars[char]["Samples"]]
+            for char in chars: char["Samples"].append(mat)
+
+            assigned.add(mat)
+                
+        # assign vials
+        for mat in vials:
+            matType = self.getMatType(mat)
+            chars = [self.chars[char] for char in self.chars if matType in self.chars[char]["SampleRole"] and len(self.chars[char]["Samples"]) < self.nPrinterSlots and mat not in self.chars[char]["Samples"]]
+            for char in chars: char["Samples"].append(mat)
+
+            assigned.add(mat)
+            
+        # assign remaining resources
+        for mat in [x for x in self.logs + self.ores + self.fish + self.bugs if x not in assigned]:
+            # print(mat)
+            matType = self.getMatType(mat)
+            try: 
+                char = [self.chars[char] for char in self.chars if self.chars[char]["SampleRole"] == matType and len(self.chars[char]["Samples"]) < self.nPrinterSlots][-1]
+                # print(char)
+                if char != {}: char["Samples"].append(mat)
+                assigned.add(mat)
+            except: 
+                continue
+            
+        # assign alchemy mats -- for hourly clicking or future vials
+        for mat in self.toolboxKeys["miscMats"]:
+            matType = self.getMatType(mat)
+            chars = [self.chars[char] for char in self.chars if matType in self.chars[char]["SampleRole"] and len(self.chars[char]["Samples"]) < self.nPrinterSlots and mat not in self.chars[char]["Samples"]]
+            for char in chars: char["Samples"].append(mat)
+
+            assigned.add(mat)
+        
+        # checking
+        print(set(self.logs + self.ores + self.fish + self.bugs).difference(assigned))
+        print(json.dumps(self.chars, indent=4))
+        for char in self.chars: 
+            if len(self.chars[char]["Samples"]) < self.nPrinterSlots:
+                print(char)
+
+    def getMatType(self, mat):
+        if mat in self.ores: return "Mining"
+        elif mat in self.logs: return "Choppin"
+        elif mat in self.bugs: return "Catching"
+        elif mat in self.fish: return "Fishing"
+        else: return "Mobs"
 
     def saveJSON(self):
         dump = json.dumps(self.userData, indent=4)
