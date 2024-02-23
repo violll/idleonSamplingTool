fa58701e154b578f407d832db079062327df33fb
violll <tri.tardis@gmail.com>
2024-02-23 14:10:30 -0500

    feat: added check if user wants to sample mobs for refinery

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from toolboxDataCollector import UserData as User
 
 class IdleonSamplingTool:
     def __init__(self) -> None:
-        user = User
-        pass
+        user = User()
+
+        # ask for refinery and vial mobs here
+        self.refinery = self.checkRefineryMobs()
+    
+    def checkRefineryMobs(self):
+        refineryMobs = input("Would you like to sample mobs for refinery? (GMush, Bullfrog, Sand, Sheep, Thermometer, Neptuneeyes)\n> ")
+        if refineryMobs in ["True", "Yes", "Y"]: return True
+        else: return False
 
 if __name__ == "__main__":
-    pass
+    IdleonSamplingTool()
